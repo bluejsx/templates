acd0672be4173f52b9d02661e7b8a844bed57008
Momijiichigo <ichigomomiji436@gmail.com>
2022-02-20 14:04:55 -0800

    decleration file for .mdx import

--- vite-mdx/src/vite-env.d.ts
+++ vite-mdx/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module '*.md' {
+  const content: string
+  export default content
+}
+declare module '*.mdx' {
+  const content: () => Blue.JSX.Element
+  export default content
+}
